71767be468d9d4bad808b89828b40f53b65e4780
Member1221 <clipseypone@gmail.com>
2019-09-10 00:07:49 +0200

    Add basic vue project

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    outputDir: "static/app/",
+    filenameHashing: false,
+    chainWebpack: config => {
+        config.plugins.delete('html')
+        config.plugins.delete('preload')
+        config.plugins.delete('prefetch')
+    }
+}

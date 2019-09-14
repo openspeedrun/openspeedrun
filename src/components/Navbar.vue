4c5ee9bb713101ffba99088cc894f0957fade045
Member1221 <clipseypone@gmail.com>
2019-09-15 01:59:57 +0200

    Create navbar as vue component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar" style="height: 64px;">
+        <a class="navItem navLogo" href="/">
+            <img src="@/assets/logo.png" \>
+        <a/>
+
+        <a class="navButton"><p>Games</p></a>
+        <a class="navButton"><p>Runs</p></a>
+        <div class="spacer" />
+
+        <slot name="user-section">USER</slot>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Navbar extends Vue {
+        @Prop() private title: string | undefined = undefined;
+
+        constructor(title: string) {
+            super();
+            this.title = title;
+        }
+    }
+</script>

1d317fa4c99b1613221bb00b3907f5340688eab6
Member1221 <clipseypone@gmail.com>
2019-09-15 02:00:08 +0200

    Create widget container as vue component

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="container-title" v-if="title !== undefined">
+            {{ title }}
+        <div/>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Container extends Vue {
+        @Prop() private title: string | undefined = undefined;
+
+        constructor(title: string) {
+            super();
+            this.title = title;
+        }
+    }
+</script>

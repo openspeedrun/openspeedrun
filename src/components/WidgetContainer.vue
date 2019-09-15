11e95943d3bf1e571862254f9c8fbf9c83080a71
Member1221 <clipseypone@gmail.com>
2019-09-15 03:59:20 +0200

    Rename Container to WidgetContainer

---
+++ src/components/WidgetContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="container-title" v-if="title !== null">
+            {{ title }}
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class WidgetContainer extends Vue {
+        title: string | null = null;
+    }
+</script>

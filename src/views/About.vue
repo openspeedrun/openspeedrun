bb7f2ce171b6b04b73a8970e95d68c8c40a1fd5f
Member1221 <clipseypone@gmail.com>
2019-09-17 23:56:26 +0200

    Add simple about page

---
+++ src/views/About.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="content">
+        <link rel="shortcut icon" href="/static/assets/favicon.png" />
+        <title>OpenSpeedRun - About</title>
+
+        <div class="contentBodyLeft" />
+
+        <div class="contentBody">
+            <widget-container title="About OSR">
+                <p>
+                    OpenSpeedRun is an open source speedrunning leaderboard.
+                    <br />
+                    The project is still in development and a lot of features will be missing for a bit.
+                </p>
+
+                <h2>Technologies Used</h2>
+
+                <br />
+
+                <h3>Backend</h3>
+                <ul>
+                    <li><a href="https://dlang.org">DLang</a></li>
+                    <li><a href="http://vibed.org/">Vibe.d</a></li>
+                </ul>
+
+                <br />
+
+                <h3>Frontend</h3>
+                <ul>
+                    <li><a href="https://www.typescriptlang.org/">Typescript</a></li>
+                    <li><a href="https://vuejs.org/">Vue.js</a></li>
+                </ul>
+            </widget-container>
+        </div>
+
+        <div class="contentBodyRight" />
+
+    </div>
+</template>
+
+<script lang="ts">
+import WidgetContainer from '@/components/WidgetContainer.vue'
+export default {
+    name: 'About',
+    components: {
+        WidgetContainer
+    }
+}
+</script>

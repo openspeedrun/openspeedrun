e5a12582e4982b9a94cef393d4eab9bcadb4c44c
Member1221 <clipseypone@gmail.com>
2019-09-17 23:54:37 +0200

    Add gradients to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="navbar" style="height: 64px;">
-        <a class="navItem navLogo" href="/">
-            <img src="@/assets/logo.png" \>
-        <a/>
+        <router-link class="navItem navLogo" v-bind:to="'/'">
+            <img src="/static/app/assets/logo.png" />
+        </router-link>
 
-        <a class="navButton"><p>Games</p></a>
-        <a class="navButton"><p>Runs</p></a>
+        <div class="nav-gradin" />
+        <a class="nav-button"><p>Games</p></a>
+        <a class="nav-button"><p>Runs</p></a>
+        <div class="nav-gradout" />
+
         <div class="spacer" />
 
-        <slot name="user-section">USER</slot>
+        <div class="nav-gradin" />
+        <slot name="user-section">
+            <a class="s-nav-button">
+                <div class="nav-item-wrapper">
+                    <div class="avatar-small">
+                        <img class="avatar-img" src="https://pbs.twimg.com/profile_images/1149808989963788289/WurVlxeK_400x400.jpg" />
+                    </div>
+                </div>
+            </a>
+        </slot>
 
     </div>
 </template>
@@ -16,13 +28,9 @@
 <script lang="ts">
     import { Component, Prop, Vue } from 'vue-property-decorator';
 
-    @Component
+    @Component({
+        name: "navbar"
+    })
     export default class Navbar extends Vue {
-        @Prop() private title: string | undefined = undefined;
-
-        constructor(title: string) {
-            super();
-            this.title = title;
-        }
     }
 </script>

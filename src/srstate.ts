4e492870ee67246b0917c90123062cacedabbfe3
Member1221 <clipseypone@gmail.com>
2019-09-25 21:38:50 +0200

    SRState Vuex module

--- src/srstate.ts
+++ src/srstate.ts
@@ -1,9 +1,30 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+import { Module as Mod } from 'vuex';
 
 @Module
 export default class SRState extends VuexModule {
-  token: string;
+    token: string | null;
+    username: string | null;
 
-  @Mutation setToken(token: string) { this.token = token; console.log("Saved JWT auth token!"); }
-  @Mutation getToken(): string { return this.token; }
-}
+    constructor(module: Mod<ThisType<any>, any>) {
+        super(module)
+        this.token = localStorage.getItem('auth');
+        this.username = localStorage.getItem('username');
+    }
+
+    @Mutation setToken(token: string | null) { 
+        this.token = token; 
+        if (typeof token === "string") {
+            localStorage.setItem('auth', token as string);
+        } else {
+            localStorage.removeItem('auth');
+            localStorage.removeItem('username')
+            this.username = null;
+        }
+    }
+
+    @Mutation setUsername(username: string | null) {
+        localStorage.setItem('username', username as string);
+        this.username = username;
+    }
+} 
